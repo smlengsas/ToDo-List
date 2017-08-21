68be3a9775915bf65ad568a90abd480d0adab772
smlengsas <smlengsas@gmail.com>
2017-08-21 15:46:42 -0400

    To Do List work

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -11,20 +11,50 @@ import './style.css';
 import './styleM.css';
 
 export default class Home extends React.PureComponent {
+
+  constructor() {
+    super();
+    this.state = {
+      listItems:[],
+      inputItem: ""
+    }
+  }
+
+  handleItem = (event) => {
+    this.setState({
+      inputItem:event.target.value
+    })
+  };
+
+  storeItem = () => {
+    var listItems = this.state.listItems;
+    var inputItem = this.state.inputItem;
+
+    if (inputItem !== "") {
+      listItems.push(inputItem);
+
+      this.setState({
+        listItems:listItems,
+        inputItem:""
+      })
+    }
+  };
+
   render() {
     return (
       <div className="container">
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
 
         <div className ="inputContainer">
-          <input type="text" className="todoInput"/>
-          <input type="submit" value="Add to List" className="todoButton"/>
+          <input type="text" className="todoInput" onChange={this.handleItem} value={this.state.inputItem}/>
+          <input type="submit" value="Add to List" className="todoButton" onClick={this.storeItem}/>
         </div>
-
         <div className="todoList">
-          <div className="listItem">
-            Here is a To - Do List Item.
-          </div>
+          {this.state.listItems.map((item, index) => (
+            <div className="listItem" key={index}>
+              {item}
+            </div>
+          ))}
         </div>
       </div>
     );
