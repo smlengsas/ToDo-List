9e4f74f7c7d036e26f35adc880cef4e33c0932f4
smlengsas <smlengsas@gmail.com>
2017-08-29 15:23:15 -0400

    Front End and Back End Project 1

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -18,6 +18,67 @@ export default class Home extends React.PureComponent {
       listItems:[],
       inputItem: ""
     }
+  };
+
+  componentWillMount() {
+    this.getTasks();
+  }
+
+  getTasks = () => {
+    fetch('http://localhost:8000/api/getTasks', {
+      method:'GET'
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      this.setState({
+        listItems:json.tasks
+      })
+    }.bind(this))
+  };
+
+  getFirst = () => {
+    fetch('http://localhost:8000/api/getTasks', {
+      method: 'GET'
+    })
+  }
+
+  storeTask = () => {
+    let data = new FormData();
+    data.append('taskContent', this.state.inputItem);
+
+    fetch('http://localhost:8000/api/storeTask', {
+      method:'POST',
+      body:data
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      let listItems = this.state.listItems;
+      listItems.push(json.task);
+      this.setState({
+        listItems:listItems,
+        inputItem:""
+      })
+      this.forceUpdate();
+    }.bind(this))
+  }
+
+  destroyOne = (id, index) => {
+    fetch('http://localhost:8000/api/destroyOne/' +id, {
+      method: 'POST',
+      mode:'no-cors'
+    })
+    .then(function() {
+      let listItems = this.state.listItems;
+      listItems.splice(index, 1);
+      this.setState({
+        listItems:listItems
+      })
+      this.forceUpdate();
+    }.bind(this))
   }
 
   handleItem = (event) => {
@@ -29,16 +90,16 @@ export default class Home extends React.PureComponent {
   handleEnter = (event) => {
     if(event.keyCode === 13)
     {
-    this.storeItem();
+    this.storeTask();
     }
-  }
+  };
 
   resetButton = (event) => {
     this.setState({
       listItems:[],
       inputItem:""
     })
-  }
+  };
 
   storeItem = () => {
     var listItems = this.state.listItems;
@@ -63,7 +124,7 @@ export default class Home extends React.PureComponent {
     else {
       item.style.textDecoration = "line-through";
     }
-  }
+  };
 
   render() {
     return (
@@ -72,16 +133,18 @@ export default class Home extends React.PureComponent {
 
         <div className ="inputContainer">
           <input type="text" className="todoInput" onChange={this.handleItem} onKeyDown={this.handleEnter}value={this.state.inputItem}/>
-          <input type="submit" value="Stuff N Thangs" className="todoButton" onClick={this.storeItem}/>
+          <input type="submit" value="Stuff N Thangs" className="todoButton" onClick={this.storeTask}/>
           <input type="reset" value ="Reset List" className="resetButton" onClick={this.resetButton}/>
         </div>
+
         <div className="todoList">
           {this.state.listItems.map((item, index) => (
-            <div className="listItem" key={index} onClick={this.strikeThrough}>
-              {item}
+            <div className="listItem" key={index} onClick={() => this.destroyOne(item.id, index)}>
+              {item.taskContent}
             </div>
           ))}
         </div>
+
       </div>
     );
   }
